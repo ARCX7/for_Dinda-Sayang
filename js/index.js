e44c959628e94b33ef48b9369ab333da4b33a600
ARCX7 <arcx7x@gmail.com>
2025-06-09 15:45:53 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `DINDA SAYANG MAS PUNYA SESUATU BUATMU`.split('')
+const text = `I Have Something`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
